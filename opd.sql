5195e9c0622af78094a869737bd48ae5281e0e04
koushikgillala <gillala001@gannon.edu>
2022-12-07 21:38:45 -0500

    Added new files

---
+++ opd.sql
@@ -0,0 +1,397 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Nov 17, 2022 at 08:16 PM
+-- Server version: 10.4.24-MariaDB
+-- PHP Version: 8.1.6
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `opd`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `categories`
+--
+
+CREATE TABLE `categories` (
+  `categorieId` int(12) NOT NULL,
+  `categorieName` varchar(255) NOT NULL,
+  `categorieDesc` text NOT NULL,
+  `categorieCreateDate` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `categories`
+--
+
+INSERT INTO `categories` (`categorieId`, `categorieName`, `categorieDesc`, `categorieCreateDate`) VALUES
+(3, 'PIZZA MANIA', 'Indulge into mouth-watering taste of Pizza mania range, perfect answer to all your food cravings', '2021-03-17 18:17:43'),
+(5, 'BEVERAGES', 'Complement your pizza with wide range of beverages available at Domino\'s Pizza India', '2021-03-17 21:58:58'),
+(7, 'BURGERs', 'Domino’s Pizza Introducing all new Burger Pizza with the tag line looks like a burger, tastes like a pizza. Burger Pizza is burger sized but comes in a small pizza box. It is come with pizza toppings such as herbs, vegetables, tomato sauce and mozzarella,', '2021-03-18 08:06:30'),
+(8, 'Drinks', 'CHOICE OF Drinks', '2021-03-18 08:13:47'),
+(11, 'VEG', 'A delight for veggie lovers! Choose from our wide range of delicious vegetarian food, it\'s softer and tastier', '2021-03-17 18:16:28'),
+(12, 'NON-VEG ', 'Choose your favourite non-veg items from the menu. Get fresh non-veg dish with your choice of crusts & toppings', '2021-03-17 18:17:14');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `contact`
+--
+
+CREATE TABLE `contact` (
+  `contactId` int(21) NOT NULL,
+  `userId` int(21) NOT NULL,
+  `email` varchar(35) NOT NULL,
+  `phoneNo` bigint(21) NOT NULL,
+  `orderId` int(21) NOT NULL DEFAULT 0 COMMENT 'If problem is not related to the order then order id = 0',
+  `message` text NOT NULL,
+  `time` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `contactreply`
+--
+
+CREATE TABLE `contactreply` (
+  `id` int(21) NOT NULL,
+  `contactId` int(21) NOT NULL,
+  `userId` int(23) NOT NULL,
+  `message` text NOT NULL,
+  `datetime` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `deliverydetails`
+--
+
+CREATE TABLE `deliverydetails` (
+  `id` int(21) NOT NULL,
+  `orderId` int(21) NOT NULL,
+  `deliveryBoyName` varchar(35) NOT NULL,
+  `deliveryBoyPhoneNo` bigint(25) NOT NULL,
+  `deliveryTime` int(200) NOT NULL COMMENT 'Time in minutes',
+  `dateTime` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `orderitems`
+--
+
+CREATE TABLE `orderitems` (
+  `id` int(21) NOT NULL,
+  `orderId` int(21) NOT NULL,
+  `pizzaId` int(21) NOT NULL,
+  `itemQuantity` int(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `orders`
+--
+
+CREATE TABLE `orders` (
+  `orderId` int(21) NOT NULL,
+  `userId` int(21) NOT NULL,
+  `address` varchar(255) NOT NULL,
+  `zipCode` int(21) NOT NULL,
+  `phoneNo` bigint(21) NOT NULL,
+  `amount` int(200) NOT NULL,
+  `paymentMode` enum('0','1') NOT NULL DEFAULT '0' COMMENT '0=cash on delivery, \r\n1=online ',
+  `orderStatus` enum('0','1','2','3','4','5','6') NOT NULL DEFAULT '0' COMMENT '0=Order Placed.\r\n1=Order Confirmed.\r\n2=Preparing your Order.\r\n3=Your order is on the way!\r\n4=Order Delivered.\r\n5=Order Denied.\r\n6=Order Cancelled.',
+  `orderDate` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pizza`
+--
+
+CREATE TABLE `pizza` (
+  `pizzaId` int(12) NOT NULL,
+  `pizzaName` varchar(255) NOT NULL,
+  `pizzaPrice` int(12) NOT NULL,
+  `pizzaDesc` text NOT NULL,
+  `pizzaCategorieId` int(12) NOT NULL,
+  `pizzaPubDate` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `pizza`
+--
+
+INSERT INTO `pizza` (`pizzaId`, `pizzaName`, `pizzaPrice`, `pizzaDesc`, `pizzaCategorieId`, `pizzaPubDate`) VALUES
+(1, 'chole paratha', 99, 'A hugely popular chole paratha, with a deliciously tangy single cheese topping\r\n\r\n', 11, '2021-03-17 21:03:26'),
+(21, 'TOMATO', 99, 'Juicy tomato in a flavourful combination with cheese I tangy sauce', 3, '2021-03-17 21:44:44'),
+(22, 'VEG LOADED', 149, 'Tomato | Grilled Mushroom |Jalapeno |Golden Corn | Beans in a fresh pan crust', 3, '2021-03-17 21:45:34'),
+(23, 'CHEESY', 99, 'Orange Cheddar Cheese I Mozzarella', 3, '2021-03-17 21:46:21'),
+(24, 'CAPSICUM', 99, 'Capsicum', 3, '2021-03-17 21:47:07'),
+(25, 'ONION', 99, 'onion', 3, '2021-03-17 21:47:51'),
+(26, 'GOLDEN CORN', 139, 'Golden Corn', 3, '2021-03-17 21:48:44'),
+(27, 'PANEER & ONION', 149, 'Creamy Paneer | Onion', 3, '2021-03-17 21:49:36'),
+(28, 'CHEESE N TOMATO', 149, 'A delectable combination of cheese and juicy tomato', 3, '2021-03-17 21:50:20'),
+(29, 'Garlic Breadsticks', 99, 'The endearing tang of garlic in breadstics baked to perfection.', 4, '2021-03-17 22:01:33'),
+(30, 'Stuffed Garlic Bread', 99, 'Freshly Baked Garlic Bread stuffed with mozzarella cheese, sweet corns & tangy and spicy jalapeños', 4, '2021-03-17 22:02:50'),
+(31, 'Veg Pasta Italiano White', 99, 'Penne Pasta tossed with extra virgin olive oil, exotic herbs & a generous helping of new flavoured sauce.', 4, '2021-03-17 22:03:44'),
+(32, 'Non Veg Pasta Italiano White', 99, 'Penne Pasta tossed with extra virgin olive oil, exotic herbs & a generous helping of new flavoured sauce.', 4, '2021-03-17 22:05:08'),
+(33, 'Cheese Jalapeno Dip', 35, 'A soft creamy cheese dip spiced with jalapeno.', 4, '2021-03-17 22:06:06'),
+(34, 'Cheese Dip', 35, 'A dreamy creamy cheese dip to add that extra tang to your snack.', 4, '2021-03-17 22:06:59'),
+(35, 'Lava Cake', 99, 'Filled with delecious molten chocolate inside.', 4, '2021-03-17 22:08:13'),
+(36, 'Butterscotch Mousse Cake', 149, 'A Creamy & Chocolaty indulgence with layers of rich, fluffy Butterscotch Cream and delicious Dark Chocolate Cake, topped with crunchy Dark Chocolate morsels - for a perfect dessert treat!', 4, '2021-03-17 22:08:58'),
+(37, 'Lipton Ice Tea', 25, ' 250ml', 5, '2021-03-17 22:12:53'),
+(38, 'Mirinda', 35, '500ml', 5, '2021-03-17 22:13:38'),
+(39, 'PEPSI BLACK CAN', 45, 'PEPSI BLACK CAN', 5, '2021-03-17 22:14:24'),
+(40, 'Pepsi\r\n', 52, '500ml', 5, '2021-03-17 22:16:29'),
+(41, '7Up', 52, '500ml', 5, '2021-03-17 22:17:08'),
+(42, 'Cheese Burst', 249, 'Crust with oodles of yummy liquid cheese filled inside.', 6, '2021-03-18 07:57:27'),
+(43, 'Classic Hand Tossed', 249, 'Dominos traditional hand stretched crust, crisp on outside, soft & light inside.', 6, '2021-03-18 07:59:52'),
+(44, 'Wheat Thin Crust', 299, 'Presenting the light healthier and delicious light wheat thin crust from Dominos.', 6, '2021-03-18 08:00:39'),
+(45, 'Fresh Pan Pizza', 299, 'Tastiest Pan Pizza.Ever.\r\nDomino’s freshly made pan-baked pizza; deliciously soft ,buttery,extra cheesy and delightfully crunchy.', 6, '2021-03-18 08:01:29'),
+(46, 'New Hand Tossed', 299, 'Classic Domino\'s crust. Fresh, hand stretched.', 6, '2021-03-18 08:03:17'),
+(47, 'BURGER PIZZA- CLASSIC VEG', 129, 'Bite into delight! Witness the epic combination of pizza and burger with our classic Burger Pizza, that looks good and tastes great!', 7, '2021-03-18 08:09:17'),
+(48, 'BURGER PIZZA- PREMIUM VEG', 139, 'The good just got better! Treat yourself to the premium taste of the Burger Pizza, that looks good and tastes great with paneer and red paprika.', 7, '2021-03-18 08:09:59'),
+(49, 'BURGER PIZZA- CLASSIC NON VEG', 149, 'For all the meat cravers, try the classic non-veg Burger Pizza loaded with the goodness of burger and the greatness of pizza.', 7, '2021-03-18 08:10:37'),
+(53, 'Packaged Drinking Water', 20, 'Drinking Water	', 5, '2021-03-18 08:20:40'),
+(100, 'Dhosa \r\n', 149, 'Dhosa\r\n\r\n', 11, '2021-03-17 21:22:07'),
+(101, 'plain rice\r\n', 49, 'plain basmati rice', 11, '2021-03-17 21:26:31'),
+(197, 'polao', 199, 'polao', 11, '2021-03-17 21:20:58'),
+(198, 'polao rice', 99, 'polao rice 400Gm', 2, '2021-03-17 21:34:37'),
+(202, 'Dal Makhani\r\n', 100, '\r\n\r\nDelicious dal makhani', 11, '2021-03-17 21:23:48'),
+(203, 'paneer butter masala\r\n', 319, 'For a vegetarian looking for a BIG treat that goes easy on the spices, this one\'s got it all.. The onions, the capsicum, those delectable mushrooms - with paneer and golden corn to top it all.\r\n\r\n', 11, '2021-03-17 21:24:38'),
+(205, 'Chicken Biriyani\r\n', 239, 'Mmm! Biriyani\r\n\r\n', 12, '2021-03-17 21:36:36'),
+(206, 'Egg Curry\r\n', 79, '4 Eggs\r\n', 12, '2021-03-17 21:37:21'),
+(207, 'Chicken Chow', 319, 'Chicken Chow with toppings', 12, '2021-03-17 21:38:13'),
+(208, 'chicken curry\r\n', 399, 'Spicy Chicken curry 300Gm', 12, '2021-03-17 21:39:49'),
+(209, 'CHICKEN FIESTA Leg piece', 199, 'Grilled Chicken leg piece (2 piece)', 12, '2021-03-17 21:40:58'),
+(300, 'CHICKEN Thali', 259, 'Chicken thali with egg and papad', 12, '2021-03-17 21:35:31'),
+(400, 'Fried rice', 249, 'fried rice with crisp capsicum and spicy red pepper - quite a mouthful!\r\n\r\n', 11, '2021-03-17 21:23:05'),
+(500, 'Lemon Juice', 50, '250 Ml', 8, '2021-03-18 08:14:52'),
+(501, 'Mohito', 60, '250ml', 8, '2021-03-18 08:15:36');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `sitedetail`
+--
+
+CREATE TABLE `sitedetail` (
+  `tempId` int(11) NOT NULL,
+  `systemName` varchar(21) NOT NULL,
+  `email` varchar(35) NOT NULL,
+  `contact1` bigint(21) NOT NULL,
+  `contact2` bigint(21) DEFAULT NULL COMMENT 'Optional',
+  `address` text NOT NULL,
+  `dateTime` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `sitedetail`
+--
+
+INSERT INTO `sitedetail` (`tempId`, `systemName`, `email`, `contact1`, `contact2`, `address`, `dateTime`) VALUES
+(1, 'E catering World', 'ecatering123@gmail.com', 2515469442, 6304468851, '601 Sherwood Ave.<br> San Bernandino', '2022-11-03 19:56:25');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(21) NOT NULL,
+  `username` varchar(21) NOT NULL,
+  `firstName` varchar(21) NOT NULL,
+  `lastName` varchar(21) NOT NULL,
+  `email` varchar(35) NOT NULL,
+  `phone` bigint(20) NOT NULL,
+  `userType` enum('0','1') NOT NULL DEFAULT '0' COMMENT '0=user\r\n1=admin',
+  `password` varchar(255) NOT NULL,
+  `joinDate` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `username`, `firstName`, `lastName`, `email`, `phone`, `userType`, `password`, `joinDate`) VALUES
+(1, 'admin', 'admin', 'admin', 'admin@gmail.com', 1111111111, '1', '$2y$10$AAfxRFOYbl7FdN17rN3fgeiPu/xQrx6MnvRGzqjVHlGqHAM4d9T1i', '2021-04-11 11:40:58'),
+(2, 'abc', 'a', 'b', '1@abc.com', 1234567890, '0', '$2y$10$bSUa9aeGGDT6lQUcpVKd4Oo1iGnPxwk/W3sVeN4XVIGNSzy3B.XMi', '2022-11-18 00:38:21');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `viewcart`
+--
+
+CREATE TABLE `viewcart` (
+  `cartItemId` int(11) NOT NULL,
+  `pizzaId` int(11) NOT NULL,
+  `itemQuantity` int(100) NOT NULL,
+  `userId` int(11) NOT NULL,
+  `addedDate` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `viewcart`
+--
+
+INSERT INTO `viewcart` (`cartItemId`, `pizzaId`, `itemQuantity`, `userId`, `addedDate`) VALUES
+(1, 1, 1, 1, '2022-11-18 00:23:15');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `categories`
+--
+ALTER TABLE `categories`
+  ADD PRIMARY KEY (`categorieId`);
+ALTER TABLE `categories` ADD FULLTEXT KEY `categorieName` (`categorieName`,`categorieDesc`);
+
+--
+-- Indexes for table `contact`
+--
+ALTER TABLE `contact`
+  ADD PRIMARY KEY (`contactId`);
+
+--
+-- Indexes for table `contactreply`
+--
+ALTER TABLE `contactreply`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `deliverydetails`
+--
+ALTER TABLE `deliverydetails`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `orderId` (`orderId`);
+
+--
+-- Indexes for table `orderitems`
+--
+ALTER TABLE `orderitems`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `orders`
+--
+ALTER TABLE `orders`
+  ADD PRIMARY KEY (`orderId`);
+
+--
+-- Indexes for table `pizza`
+--
+ALTER TABLE `pizza`
+  ADD PRIMARY KEY (`pizzaId`);
+ALTER TABLE `pizza` ADD FULLTEXT KEY `pizzaName` (`pizzaName`,`pizzaDesc`);
+
+--
+-- Indexes for table `sitedetail`
+--
+ALTER TABLE `sitedetail`
+  ADD PRIMARY KEY (`tempId`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`) USING BTREE,
+  ADD UNIQUE KEY `username` (`username`);
+
+--
+-- Indexes for table `viewcart`
+--
+ALTER TABLE `viewcart`
+  ADD PRIMARY KEY (`cartItemId`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `categories`
+--
+ALTER TABLE `categories`
+  MODIFY `categorieId` int(12) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
+
+--
+-- AUTO_INCREMENT for table `contact`
+--
+ALTER TABLE `contact`
+  MODIFY `contactId` int(21) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `contactreply`
+--
+ALTER TABLE `contactreply`
+  MODIFY `id` int(21) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `deliverydetails`
+--
+ALTER TABLE `deliverydetails`
+  MODIFY `id` int(21) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `orderitems`
+--
+ALTER TABLE `orderitems`
+  MODIFY `id` int(21) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `orders`
+--
+ALTER TABLE `orders`
+  MODIFY `orderId` int(21) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `pizza`
+--
+ALTER TABLE `pizza`
+  MODIFY `pizzaId` int(12) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=502;
+
+--
+-- AUTO_INCREMENT for table `sitedetail`
+--
+ALTER TABLE `sitedetail`
+  MODIFY `tempId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(21) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `viewcart`
+--
+ALTER TABLE `viewcart`
+  MODIFY `cartItemId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
